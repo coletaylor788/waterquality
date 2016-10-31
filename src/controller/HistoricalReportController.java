63de02aca2bb48b4bda1479d43594a319ba61b65
Greenness <patrickblack154@gmail.com>
2016-10-31 19:37:17 -0400

    Added combobox to fxml and controller

--- src/controller/HistoricalReportController.java
+++ src/controller/HistoricalReportController.java
@@ -1,20 +1,47 @@
 package controller;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class HistoricalReportController {
 
     @FXML
-    TextField latitude;
+    private TextField latitude;
 
     @FXML
-    TextField longitude;
+    private TextField longitude;
 
     @FXML
-    TextField year;
+    private TextField year;
 
+    @FXML
+    private LineChart<Integer, Integer> graph;
+
+    @FXML
+    private ComboBox<String> ppm;
+
+
+
+    @FXML
+    /**
+     * populates combo box
+     */
+    private void initialize() {
+        ppm.getItems().addAll(generatePPM());
+    }
+
+
+    private ObservableList<String> generatePPM() {
+        ObservableList<String> strList = FXCollections.observableArrayList();
+        strList.add("Virus PPM");
+        strList.add("Containment PPM");
+        return strList;
+    }
 
     /**
      * handles Cancel button
