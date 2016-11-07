ffa5af43fb6628391dcf445a5fd93a1ef65c3e4d
coletaylor788 <coletaylor788@gmail.com>
2016-11-07 12:38:47 -0500

    Adding my JUNIT test file

---
+++ src/test/ColeCreateUserTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Before;
+
+/**
+ * Tests the creation of a User
+ *
+ * @author Cole Taylor
+ * @version 1.0
+ */
+public class ColeCreateUserTest {
+
+    @Before
+    public void setup() {
+
+    }
+}
