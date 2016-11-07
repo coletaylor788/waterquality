8701c574f5189dc5d4b44fe01a462a3725f902ec
Cole Taylor <coletaylor788@gmail.com>
2016-11-07 14:33:33 -0500

    Finalizing create user JUNIT test

--- src/test/ColeCreateUserTest.java
+++ src/test/ColeCreateUserTest.java
@@ -1,13 +1,12 @@
 package test;
 
-import model.Facade;
 import model.auth.Role;
 import model.auth.State;
 import model.auth.User;
 import model.auth.exceptions.AuthenticationException;
+import model.auth.exceptions.InvalidEmailException;
 import model.auth.exceptions.UnableToHashPasswordException;
 import model.exceptions.EmptyRequiredFieldException;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -18,11 +17,9 @@ import org.junit.Test;
  */
 public class ColeCreateUserTest {
 
-    @Before
-    public void setup() {
-
-    }
-
+    /**
+     * Tests a valid call to create a user
+     */
     @Test
     public void testValidParameters() {
         User u = null;
@@ -36,6 +33,10 @@ public class ColeCreateUserTest {
                 "address", "city", State.ALABAMA, 12345));
     }
 
+    /**
+     * Test creating a user with an empty password
+     * @throws EmptyRequiredFieldException signaling password is empty
+     */
     @Test(expected = EmptyRequiredFieldException.class)
     public void testEmptyPassword() throws EmptyRequiredFieldException {
         User u = null;
@@ -47,6 +48,10 @@ public class ColeCreateUserTest {
         }
     }
 
+    /**
+     * Tests creating a user with an empty first name
+     * @throws EmptyRequiredFieldException signaling first name is empty
+     */
     @Test(expected = EmptyRequiredFieldException.class)
     public void testEmptyFirstName() throws EmptyRequiredFieldException {
         User u = null;
@@ -58,6 +63,10 @@ public class ColeCreateUserTest {
         }
     }
 
+    /**
+     * Tests creating a user with an empty last name
+     * @throws EmptyRequiredFieldException signaling last name is empty
+     */
     @Test(expected = EmptyRequiredFieldException.class)
     public void testEmptyLastName() throws EmptyRequiredFieldException {
         User u = null;
@@ -69,6 +78,168 @@ public class ColeCreateUserTest {
         }
     }
 
+    /**
+     * Tests creating a user with an empty role
+     * @throws EmptyRequiredFieldException signaling role is empty
+     */
+    @Test(expected = EmptyRequiredFieldException.class)
+    public void testEmptyRole() throws EmptyRequiredFieldException {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", null, "c@c.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (AuthenticationException e) {
+            assert(false);
+        }
+    }
+
+    /**
+     * Tests creating a user with role = WORKER
+     */
+    @Test
+    public void testWorkerRole() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.WORKER, "c@c.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+        assert(checkUser(u, "user", "password", "first", "last", Role.WORKER, "c@c.com", "title",
+                "address", "city", State.ALABAMA, 12345));
+    }
+
+    /**
+     * Tests creating a user with role = MANAGER
+     */
+    @Test
+    public void testManagerRole() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.MANAGER, "c@c.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+        assert(checkUser(u, "user", "password", "first", "last", Role.MANAGER, "c@c.com", "title",
+                "address", "city", State.ALABAMA, 12345));
+    }
+
+    /**
+     * Tests creating a user with role = ADMIN
+     */
+    @Test
+    public void testAdminRole() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.ADMIN, "c@c.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+        assert(checkUser(u, "user", "password", "first", "last", Role.ADMIN, "c@c.com", "title",
+                "address", "city", State.ALABAMA, 12345));
+    }
+
+    /**
+     * Tests creating a user with an email missing the '@' character
+     */
+    @Test
+    public void testEmailWithoutAt() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.USER, "cc.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (InvalidEmailException e) {
+            assert(true);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+    }
+
+    /**
+     * Tests creating a user with an email missing characters before the '@' symbol
+     */
+    @Test
+    public void testEmailWithoutPrefix() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.USER, "@c.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (InvalidEmailException e) {
+            assert(true);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+    }
+
+    /**
+     * Tests creating a user with an email missing the domain name. (i.e. gmail, gatech)
+     */
+    @Test
+    public void testEmailWithoutDomain() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.USER, "c@.com", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (InvalidEmailException e) {
+            assert(true);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+    }
+
+    /**
+     * Tests creating a user with an email missing the domain suffix (i.e. com, org, edu)
+     */
+    @Test
+    public void testEmailWithoutSuffix() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.USER, "c@c.", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (InvalidEmailException e) {
+            assert(true);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            assert(false);
+        }
+    }
+
+    /**
+     * Tests creating a user with an email missing the dot
+     */
+    @Test
+    public void testEmailWithoutDot() {
+        User u = null;
+        try {
+            u = new User("user", "password", "first", "last", Role.USER, "c@ccom", "title",
+                    "address", "city", State.ALABAMA, 12345);
+        } catch (InvalidEmailException e) {
+            assert(true);
+        } catch (AuthenticationException | EmptyRequiredFieldException e) {
+            System.out.println(e);
+            assert(false);
+        }
+    }
+
+    /**
+     * Checks the user against a series of arguments
+     *
+     * @param u is the user to check against
+     * @param username is the username to match
+     * @param password is the password to match
+     * @param first is the first name to match
+     * @param last is the last name to match
+     * @param role is the role to match
+     * @param email is the email to match
+     * @param title is the title to match
+     * @param address is the address to match
+     * @param city is the city to match
+     * @param state is the state to match
+     * @param zip is the zip code to match
+     *
+     * @return true if the fields of u equal the provided arguments, false otherwise
+     */
     private boolean checkUser(User u, String username, String password, String first, String last,
                               Role role, String email, String title, String address, String city,
                               State state, int zip) {
