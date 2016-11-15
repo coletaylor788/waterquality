7351546552e14196e14db75b06ba886a9272feff
Nkosi Kee <nkosi.kee@gatech.edu>
2016-11-15 01:36:58 -0500

    Attempting to fix Test

--- src/test/NkosiTest.java
+++ src/test/NkosiTest.java
@@ -6,9 +6,6 @@ import model.auth.exceptions.AuthenticationException;
 import model.exceptions.EmptyRequiredFieldException;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import model.exceptions.EmptyRequiredFieldException;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 
 /**
  * Created by Nkosi Kee on 11/14/2016.
@@ -18,13 +15,14 @@ public class NkosiTest {
     @Test
     public void properUserSetLastNameSet() throws AuthenticationException, EmptyRequiredFieldException {
         User user = new User("Km22","3040322","Nkosi","Kee",Role.USER,"juan@gogo.com");
-        user.setLastName("Bart");
-        assertEqauls("Last Name must be Bart", "Bart", user.getLastName());
+        String bart = "Bart"
+        user.setLastName(bart);
+        assertEqauls("Last Name must be Bart", bart, user.getLastName());
     }
 
     @Test (expected = EmptyRequiredFieldException.class)
     public void testEmptyLastName() throws AuthenticationException, EmptyRequiredFieldException {
         User user = new User("Km22","3040322","Nkosi","Kee",Role.USER,"juan@gogo.com");
-        user.setLastName("Bart");
+        user.setLastName(23);
     }
 }
